e26897c40c9d78fb72e57d1e82199597c1a23886
James Grasso <grassojames5@gmail.com>
2020-12-19 22:19:48 +1100

    Added test command

--- bot/jamesina-bot/Commands/Modules/AdminCmd.cs
+++ bot/jamesina-bot/Commands/Modules/AdminCmd.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace jamesina_bot.Commands.Modules
 {
-	class Class1
+
+	[RequireUserPermission(Discord.GuildPermission.Administrator)]
+	[Group("Admin")]
+	public class AdminCmd : ModuleBase<SocketCommandContext>
 	{
+
+		[Command("Shutdown")]
+		public async Task ShutdownAsync()
+		{
+
+		}
+
 	}
 }
