d0d65dd861800a27a75a5f6f4a58f7c2fceeb51f
jamesina <61040004+jamesinaxx@users.noreply.github.com>
2021-03-01 16:50:27 +1100

    Update uhm.js

--- code/javascripts/uhm.js
+++ code/javascripts/uhm.js
@@ -1,7 +1,7 @@
 // Look... if you can get this into someone's chrome console... it'll crash their webpage in like a minute (change the 10000 to something smaller if you want it to crash quicker)
 let i = "crash "; 
 function thisIsDumb() { 
-  document.getElementsByTagName('p')[0].textContent = i; 
+  document.getElementsByTagName('*')[0].textContent = i; 
   i = i + i; 
 } 
 
