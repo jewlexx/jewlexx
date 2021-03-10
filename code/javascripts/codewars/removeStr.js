82ebe026cd786c2ef79a5449d9e78de45c9efe3c
jamesinaxx <grassojames5@gmail.com>
2021-03-10 21:18:23 +1100

    Added removestr and remove vowel... both of which work for once

--- code/javascripts/codewars/removeStr.js
+++
@@ -1,8 +0,0 @@
-/**
- * Removes any strings from the array including those which are just numbers in string forms
- * @param {any[]} l
- * @returns {number[]}
- */
-const filterList = (l) => l.filter((value) => typeof value === 'number');
-
-export default filterList;
