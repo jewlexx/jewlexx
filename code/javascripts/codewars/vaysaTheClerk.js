74d6541ada885da2ea3c0d8d9b4e5e09ea2373ca
jamesinaxx <grassojames5@gmail.com>
2021-03-10 19:39:12 +1100

    "Finished" vaysa

--- code/javascripts/codewars/vaysaTheClerk.js
+++ code/javascripts/codewars/vaysaTheClerk.js
@@ -1,25 +1,46 @@
+// Yeah nah this didn't work
 /**
  * If the amount in the array complicated maths etc ugh
  * This was just for codewars and is almost pointless outside of it
- * @param {Array} line
+ *
+ * **This didn't work out and I could not find a solution**
+ * @param {Number[]} line
  */
-
-// TODO Re-read the question you dumb idiot...
 function tickets(line) {
-  let total = 25;
+  let numberOf25 = 0;
+  let numberOf50 = 0;
+  let numberOf100 = 0;
   let succeed = true;
   line.forEach((value) => {
-    console.log(total);
-    if (total - value >= 0) {
-      total += value;
-    } else {
-      succeed = false;
+    // console.log("Just got given a " + value);
+    switch (value) {
+      case 25:
+        numberOf25++;
+        break;
+      case 50:
+        if (numberOf25 >= 1) {
+          numberOf25--;
+          numberOf50++;
+        } else {
+          succeed = false;
+        }
+        break;
+      case 100:
+        if (numberOf25 >= 3) {
+          numberOf100++;
+          numberOf25 - 3;
+        } else if (numberOf50 >= 1 && numberOf25 >= 1) {
+          numberOf100++;
+          numberOf50 - 2;
+          numberOf25 - 1;
+        } else {
+          succeed = false;
+        }
+        break;
     }
+    // console.log(numberOf25 + " " + numberOf50 + " " + numberOf100);
   });
-  return total > 0 && succeed ? "YES" : "NO";
+  return succeed ? "YES" : "NO";
 }
 
-console.log(tickets([25, 25, 50, 50]));
-console.log(tickets([25, 100]));
-
-// export { tickets };
+export { tickets };
