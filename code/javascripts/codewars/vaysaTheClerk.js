7b542343fdeeb1683a2f2fdc023358b3067ac275
jamesinaxx <grassojames5@gmail.com>
2021-03-10 19:11:42 +1100

    Updated it

--- code/javascripts/codewars/vaysaTheClerk.js
+++ code/javascripts/codewars/vaysaTheClerk.js
@@ -3,10 +3,13 @@
  * This was just for codewars and is almost pointless outside of it
  * @param {Array} line
  */
+
+// TODO Re-read the question you dumb idiot...
 function tickets(line) {
   let total = 25;
   let succeed = true;
   line.forEach((value) => {
+    console.log(total);
     if (total - value >= 0) {
       total += value;
     } else {
