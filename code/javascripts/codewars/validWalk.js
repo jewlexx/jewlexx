0e975d016e1ed12a7bf54eac5bf331a2a6326b2b
jamesinaxx <grassojames5@gmail.com>
2021-03-10 19:05:14 +1100

    Added workspace dictionary, Added clerk javascript file and added uselessness comment to existing file

--- code/javascripts/codewars/validWalk.js
+++ code/javascripts/codewars/validWalk.js
@@ -1,6 +1,7 @@
 /**
  * Checks to see if the values in the array will return you to the same location in 10 minutes assuming one value is 1 minute and the only values available are,
  * N, S, E and W being North, South, East and West respectively
+ * This was just for codewars and is almost pointless outside of it
  * @param {array} walk
  * @returns {Boolean} If the walk is valid or not
  */
---
+++ code/javascripts/codewars/vaysaTheClerk.js
@@ -0,0 +1,22 @@
+/**
+ * If the amount in the array complicated maths etc ugh
+ * This was just for codewars and is almost pointless outside of it
+ * @param {Array} line
+ */
+function tickets(line) {
+  let total = 25;
+  let succeed = true;
+  line.forEach((value) => {
+    if (total - value >= 0) {
+      total += value;
+    } else {
+      succeed = false;
+    }
+  });
+  return total > 0 && succeed ? "YES" : "NO";
+}
+
+console.log(tickets([25, 25, 50, 50]));
+console.log(tickets([25, 100]));
+
+// export { tickets };
