5b920403fb02f3fe427200e2ace3a4d172a5a41c
jamesinaxx <grassojames5@gmail.com>
2021-03-10 21:32:14 +1100

    Added everything to an index module

--- code/javascripts/codewars/jadenCase.js
+++ code/javascripts/codewars/jadenCase.js
@@ -2,7 +2,7 @@
  * Turns the given string into Jaden Case
  * @returns {string} The given string in Jaden Case
  */
-String.prototype.toJadenCase = function () {
+String.toJadenCase = function () {
   let mainArray = [];
   this.split(" ").forEach((string) => {
     mainArray.push(string.charAt(0).toUpperCase() + string.slice(1));
