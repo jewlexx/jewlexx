1e25d8621004559709286509002869c747a5c381
jamesinaxx <grassojames5@gmail.com>
2021-03-11 08:21:34 +1100

    Updated isisogram

--- code/javascripts/codewars/isogram.js
+++ code/javascripts/codewars/isogram.js
@@ -1,1 +1,16 @@
-function isIsogram(str) {}
+/**
+ *
+ * @param {string} str
+ */
+function isIsogram(str) {
+  str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    const tempString = str.replace(str[i], "");
+    if (tempString.includes(str[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isIsogram("isogram"));
