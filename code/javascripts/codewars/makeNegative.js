59946a288a57fc1c84e3b7728895544f2c435052
jamesinaxx <grassojames5@gmail.com>
2021-03-10 12:40:24 +1100

    Added makenegative.js

--- code/javascripts/codewars/makeNegative.js
+++
@@ -1,8 +0,0 @@
-/**
- * Makes the given number negative
- * @param {Number} num
- * @returns {Number} The given number as a negative
- */
-const makeNegative = (num) => (num < 0 ? num : -num);
-
-export default makeNegative;
