477b92d90ec13c4f4291271ba19f9d13f0756d6f
jamesinaxx <grassojames5@gmail.com>
2021-03-11 19:36:29 +1100

    Fixed scandir script

--- scripts/scanDir.js
+++ scripts/scanDir.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
+if (!fs.existsSync(path.join(__dirname, "../docs/assets/dist/files.txt"))) {
+  fs.mkdirSync(path.join(__dirname, "../docs/assets/dist/"));
+}
+
 fs.writeFileSync(
   path.join(__dirname, "../docs/assets/dist/files.txt"),
   fs
