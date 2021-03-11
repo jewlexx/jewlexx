5e0a510ad8c7d8bfa2a35296be568c8140a5327b
jamesinaxx <grassojames5@gmail.com>
2021-03-11 11:53:30 +1100

    Is not working :(

--- docs/scripts/webpack-scripts/webpack.config.js
+++ docs/scripts/webpack-scripts/webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: path.join(__dirname, '../index.js'),
-    output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, '../dist'),
-    },
-}
+  entry: path.join(__dirname, "../index.js"),
+  watch: true,
+  node: {
+    global: true,
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+};
