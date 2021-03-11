6db116cb943ba6b771e2cf78cd30fffe56381b77
jamesinaxx <grassojames5@gmail.com>
2021-03-11 20:17:04 +1100

    Added broken names to broken files

--- docs/assets/web/javascripts/index.js
+++ docs/assets/web/javascripts/index.js
@@ -10,9 +10,13 @@ console.log("Initialized javascript");
 function dataStuff(data) {
   console.log("List of files:\n" + data);
   data.split(" | ").forEach((fileName) => {
+    let color = "#4e5d94";
+    if (fileName.includes("broken")) {
+      color = "#FF0000";
+    }
     $(".hide-on-load").hide();
     $(".file-nav").append(
-      `<a href="https://github.com/jamesinaxx/jamesinaxx/blob/public/code/javascripts/codewars/${fileName}" target="_blank">` +
+      `<a href="https://github.com/jamesinaxx/jamesinaxx/blob/public/code/javascripts/codewars/${fileName}" target="_blank" style="color: ${color}">` +
         fileName.replace(".js", "") +
         "</a>" +
         "<br>"
