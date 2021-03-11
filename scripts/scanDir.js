e0a5f87e7035ed3c7772a6487513e0bc9b93de49
jamesinaxx <grassojames5@gmail.com>
2021-03-11 14:19:56 +1100

    Moved files.txt

--- scripts/scanDir.js
+++ scripts/scanDir.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 fs.writeFileSync(
-  path.join(__dirname, "./files.txt"),
+  path.join(__dirname, "../files.txt"),
   fs
     .readdirSync(path.join(__dirname, "../code/javascripts/codewars"))
     .join(" | ")
