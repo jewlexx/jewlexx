5b31958ccc8d8eacccd4f98b1bd797e6074cd2f8
jamesinaxx <grassojames5@gmail.com>
2021-03-12 18:39:35 +1100

    Fixed the highlighting of colours when hovered

--- docs/assets/web/javascripts/index.js
+++ docs/assets/web/javascripts/index.js
@@ -1,6 +1,5 @@
 import "../styles/index.scss";
 import $ from "jquery";
-// import "./components/FileList.jsx";
 
 console.log("Initialized javascript");
 
@@ -11,13 +10,11 @@ console.log("Initialized javascript");
 function dataStuff(data) {
   console.log("List of files:\n" + data);
   data.split(" | ").forEach((fileName) => {
-    let color = "#4e5d94";
-    if (fileName.includes("broken")) {
-      color = "#FF0000";
-    }
+    let classList = "js-codewars-file";
+    if (fileName.includes("broken")) classList = "js-codewars-broken";
     $(".hide-on-load").hide();
     $(".file-nav").append(
-      `<a href="https://github.com/jamesinaxx/jamesinaxx/blob/public/code/javascripts/codewars/${fileName}" target="_blank" style="color: ${color}">` +
+      `<a href="https://github.com/jamesinaxx/jamesinaxx/blob/public/code/javascripts/codewars/${fileName}" target="_blank" class="${classList}">` +
         fileName.replace(".js", "") +
         "</a>" +
         "<br>"
