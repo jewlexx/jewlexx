b544fd888f9f56fed5b6e6397fabf31e84280a01
jamesinaxx <grassojames5@gmail.com>
2021-03-25 22:24:42 +1100

    Feat: Added triangle.cpp as well as .vscode which is irrelevant but yano

---
+++ code/cpp/learning/triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "& & & & & &\n& & & & &\n& & & &\n& & &\n& &\n&";
+
+    /* This works too ig
+    cout << "& & & & & &" << endl;
+    cout << "& & & & &" << endl;
+    cout << "& & & &" << endl;
+    cout << "& & &" << endl;
+    cout << "& &" << endl;
+    cout << "&";
+  */
+}
