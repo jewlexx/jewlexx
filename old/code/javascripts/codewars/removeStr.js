665bbe61e1514c933bb38f160ec3aa268e91dcbd
jamesinaxx <grassojames5@gmail.com>
2021-03-25 19:35:00 +1100

    Fix: Moved to .7z file cuz 'twas being weird

--- old/code/javascripts/codewars/removeStr.js
+++
@@ -1,8 +0,0 @@
-/**
- * Removes any strings from the array including those which are just numbers in string forms
- * @param {any[]} l
- * @returns {number[]}
- */
-const filter_list = (l) => l.filter((value) => typeof value === "number");
-
-export { filter_list as filter };
