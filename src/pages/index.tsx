65918bc9972da561a8f5959cca01f969d3a95517
jamesinaxx <grassojames5@gmail.com>
2021-05-19 14:38:19 +1000

    Removed a bunch of stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils/utils.module.scss';
-import Link from 'next/link';
-import Date from '../components/date';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
-import $ from 'jquery';
 
 export default class Home extends React.Component {
 	constructor(props) {
@@ -22,7 +17,8 @@ export default class Home extends React.Component {
 				<section className={utilStyles.headingMd}>
 					<p className="bio">
 						I am a learning developer currently learning front-end
-						web development
+						web development - This is a WIP but I'm not working on
+						it...
 					</p>
 				</section>
 			</Layout>
