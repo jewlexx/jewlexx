75e5b88b44aaee865d71ad12c7429825ea5eeda4
jamesinaxx <grassojames5@gmail.com>
2021-05-25 15:02:25 +1000

    Added affirmations.json

---
+++ src/pages/api/affirmations.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import path from 'path';
+
+const affirmations = fs.readFileSync(path.join(__dirname, '..', '..'));
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	fs.writeFileSync(
+		'C:\\1dev\\jamesinaxx\\src\\pages\\api' + '\\test.json',
+		'{ "testing": "test" }'
+	);
+	res.status(200).json({ name: __dirname });
+}
