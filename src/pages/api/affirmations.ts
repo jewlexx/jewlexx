002aa249235e2e3e7f29b113b9924e32c44327d9
jamesinaxx <grassojames5@gmail.com>
2021-05-25 15:12:55 +1000

    Added error messages for non GET requests

--- src/pages/api/affirmations.ts
+++ src/pages/api/affirmations.ts
@@ -5,14 +5,20 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const affirmations: string[] = (
-		await axios.get(
-			'https://raw.githubusercontent.com/jamesinaxx/jamesinaxx/public/affirmations.json'
-		)
-	).data.affirmations;
+	if (req.method === 'GET') {
+		const affirmations: string[] = (
+			await axios.get(
+				'https://raw.githubusercontent.com/jamesinaxx/jamesinaxx/public/affirmations.json'
+			)
+		).data.affirmations;
 
-	res.status(200).json({
-		affirmation:
-			affirmations[Math.floor(Math.random() * affirmations.length)],
-	});
+		res.status(200).json({
+			affirmation:
+				affirmations[Math.floor(Math.random() * affirmations.length)],
+		});
+	} else {
+		res.status(405).json({
+			error: 'We do not support ANY requests other then "GET" yet',
+		});
+	}
 }
