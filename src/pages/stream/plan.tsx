1fa4d056637ff0453e299cf54862965391f43be2
jamesinaxx <grassojames5@gmail.com>
2021-06-09 11:45:23 +1000

    Okay... So that works ig

--- src/pages/stream/plan.tsx
+++ src/pages/stream/plan.tsx
@@ -5,9 +5,34 @@ import { connectToDatabase } from '../../util/mongodb';
 import dayjs from 'dayjs';
 
 export default function Plan({ streams }: { streams: any[] }) {
+	const today = streams.filter(
+		stream => stream.date.split(' ')[0] === dayjs().format('MM-DD-YYYY')
+	);
+
+	const other = streams.filter(
+		stream => stream.date.split(' ')[0] !== dayjs().format('MM-DD-YYYY')
+	);
+
 	return (
 		<Layout>
-			{streams.map((stream, i) => (
+			<h1 style={{ marginLeft: '16px' }}>Streams Today</h1>
+			{today.map((stream, i) => (
+				<div key={i} className={styles.streamCard}>
+					<Image
+						src={`https://static-cdn.jtvnw.net/ttv-boxart/${stream.game}.jpg?1622287396`}
+						width={62.5}
+						height={87}
+						alt={stream.game}
+						className={styles.gameImage}
+					></Image>
+					<div className={styles.streamInfo}>
+						<h1>{stream.description}</h1>
+						<h3>{dayjs(stream.date).format('LLL')}</h3>
+					</div>
+				</div>
+			))}
+			<h1 style={{ marginLeft: '16px' }}>Other</h1>
+			{other.map((stream, i) => (
 				<div key={i} className={styles.streamCard}>
 					<Image
 						src={`https://static-cdn.jtvnw.net/ttv-boxart/${stream.game}.jpg?1622287396`}
