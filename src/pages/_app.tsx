81d94d66e4a910da45b0ab935c7be261746c6dc4
jamesinaxx <grassojames5@gmail.com>
2021-06-09 11:08:33 +1000

    Fixed a few things with db and added template doc because I will forget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/global.scss';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 
 export default function App({ Component, pageProps }) {
+	dayjs.extend(advancedFormat);
+	dayjs.extend(localizedFormat);
+
 	return <Component {...pageProps} />;
 }
--- src/pages/stream/plan.tsx
+++ src/pages/stream/plan.tsx
@@ -1,6 +1,36 @@
 import styles from '../../styles/stream.module.scss';
 import Layout from '../../components/Layout/_Main';
+import { connectToDatabase } from '../../util/mongodb';
+import dayjs from 'dayjs';
 
-export default function Plan() {
-	return <Layout>{null}</Layout>;
+export default function Plan({ streams }: { streams: any[] }) {
+	return (
+		<Layout>
+			{streams.map((stream, i) => (
+				<ul key={i}>
+					<h1>{stream.description}</h1>
+					<h2>{stream.game}</h2>
+					<h3>{dayjs(stream.date).format('LLL')}</h3>
+				</ul>
+			))}
+		</Layout>
+	);
+}
+
+export async function getServerSideProps(context) {
+	const { db } = await connectToDatabase();
+
+	await db.collection('streams').deleteMany({});
+	await db.collection('streams').insertOne({
+		game: 'Minecraft',
+		description: 'Test',
+		date: '06-09-2021 10:06',
+		endTime: null,
+	});
+
+	const streams = await db.collection('streams').find({}).toArray();
+
+	return {
+		props: { streams: JSON.parse(JSON.stringify(streams)) },
+	};
 }
