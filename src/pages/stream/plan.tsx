c82d28d42f25e2a3754a2bd2563827719da87206
jamesinaxx <grassojames5@gmail.com>
2021-06-11 20:19:50 +1000

    Added unauthenticated user page

--- src/pages/stream/plan.tsx
+++ src/pages/stream/plan.tsx
@@ -3,6 +3,7 @@ import styles from '../../styles/stream.module.scss';
 import Layout from '../../components/Layout/_Main';
 import { connectToDatabase } from '../../util/mongodb';
 import dayjs from 'dayjs';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Plan({ streams }: { streams: any[] }) {
 	const today = streams.filter(
@@ -12,6 +13,61 @@ export default function Plan({ streams }: { streams: any[] }) {
 	const other = streams.filter(
 		stream => stream.date.split(' ')[0] !== dayjs().format('MM-DD-YYYY')
 	);
+
+	const { user, error, isLoading } = useUser();
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
+
+	if ((user || { name: null }).name !== 'jamesinaxx')
+		return (
+			<div className='bgimg'>
+				<div className='middle'>
+					<h1>COMING SOON</h1>
+					<hr />
+					<p>Date is also coming soon... Maybe</p>
+				</div>
+				<div className='bottomleft'>
+					<p>jamesinaxx</p>
+				</div>
+				<style jsx>{`
+					body,
+					html {
+						height: 100%;
+						background: #000;
+					}
+
+					/* Position text in the top-left corner */
+					.topleft {
+						position: absolute;
+						top: 0;
+						left: 16px;
+					}
+
+					/* Position text in the bottom-left corner */
+					.bottomleft {
+						position: absolute;
+						bottom: 0;
+						left: 16px;
+					}
+
+					/* Position text in the middle */
+					.middle {
+						position: absolute;
+						top: 50%;
+						left: 50%;
+						transform: translate(-50%, -50%);
+						text-align: center;
+					}
+
+					/* Style the <hr> element */
+					hr {
+						margin: auto;
+						width: 40%;
+					}
+				`}</style>
+			</div>
+		);
 
 	return (
 		<Layout>
