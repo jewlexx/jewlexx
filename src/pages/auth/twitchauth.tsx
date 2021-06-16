ba3b27795c0558155d1cb9f9c5887ec7d24b7041
jamesinaxx <grassojames5@gmail.com>
2021-06-16 02:41:05 +0000

    pleaseeee

--- src/pages/auth/twitchauth.tsx
+++ src/pages/auth/twitchauth.tsx
@@ -1,102 +1,126 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Component } from 'react';
 
-export default function TwitchAuth({clientId, redirectUrl}) {
-	let urlParams: URLSearchParams = null;
+export default class TwitchAuth extends Component<
+	{ clientId: string; redirectUrl: string },
+	{ urlParams: any, authUrl: URL }
+> {
+	constructor(props) {
+		super(props);
 
-	useEffect(() => {
-		const queryString = window.location.hash.replace('#', '?');
-		urlParams = new URLSearchParams(queryString);
-		const redirect_uri = redirectUrl + '/auth/twitchauth';
+		this.state = {
+			urlParams: null,
+			authUrl: null,
+		};
+	}
 
-		authUrl.searchParams.append('client_id', clientId);
+	componentDidMount() {
+		const authUrl = new URL('https://id.twitch.tv/oauth2/authorize');
+
+		const hash = window.location.hash.substr(1);
+
+		const result = hash.split('&').reduce(function (res, item) {
+			var parts = item.split('=');
+			res[parts[0]] = parts[1];
+			return res;
+		}, {});
+
+		this.setState({urlParams: result});
+
+		const redirect_uri = this.props.redirectUrl + '/auth/twitchauth';
+
+		authUrl.searchParams.append('client_id', this.props.clientId);
 		authUrl.searchParams.append('redirect_uri', redirect_uri);
 		authUrl.searchParams.append('response_type', 'token');
 		authUrl.searchParams.append('scope', 'user:read:follows');
-	});
 
-	const authUrl = new URL('https://id.twitch.tv/oauth2/authorize');
+		this.setState({authUrl})
+	}
 
-	const TwitchIcon = () => (
-		<img
-			src='/images/TwitchGlitchPurple.svg'
-			width='24px'
-			height='24px'
-		></img>
-	);
+	render() {
+		const TwitchIcon = () => (
+			<img
+				src='/images/TwitchGlitchPurple.svg'
+				width='24px'
+				height='24px'
+			></img>
+		);
 
-	const Button = () => (
-		<div className='twitchLogin'>
-			{!(urlParams || { has: () => null }).has('access_token') ? (
-				<p>
-					Click here to{' '}
-					<button onClick={() => window.open(authUrl.href, '_self')}>
-						Login with Twitch{' '}
-						<i>
-							<TwitchIcon />
-						</i>
-					</button>
-				</p>
-			) : (
-				<p>
-					Thank you for authorizing with Twitch! Open the extension on
-					this page to begin using it!
-				</p>
-			)}
-		</div>
-	);
-
-	return (
-		<>
-			<style jsx global>{`
-				body {
-					font-family: 'Akaya Telivigala', cursive;
-					background: #23272a;
-					color: #fff;
-					display: inline-block;
-					margin: 0px auto;
-					text-align: center;
-				}
-
-				.twitchLogin {
-					-ms-transform: translate(-50%, -50%);
-					transform: translate(-50%, -50%);
-					transition: background-color 0.125s ease-in-out;
-					transform-origin: center center;
-					margin: 0;
-					position: absolute;
-					top: 50%;
-					left: 50%;
-
-					font-size: xx-large;
-				}
-
-				button {
-					font-family: 'Roboto Mono', monospace;
-					border-style: solid;
-					font-size: xx-large;
-					border-radius: 5px;
-					border-color: #9146ff;
-					background-color: #f0f0ff;
-				}
-
-				button:hover {
-					background-color: #383f5a;
-				}
-			`}</style>
-			<div>
-				This is in pre-pre-pre-pre-pre production. If you are not
-				jamesinaxx, don't use this...
-				<Button />
+		const Button = () => (
+			<div className='twitchLogin'>
+				{!this.state.urlParams ? (
+					<p>
+						Click here to{' '}
+						<button
+							onClick={() => window.open(this.state.authUrl.href, '_self')}
+						>
+							Login with Twitch{' '}
+							<i>
+								<TwitchIcon />
+							</i>
+						</button>
+					</p>
+				) : (
+					<p>
+						Thank you for authorizing with Twitch! Open the
+						extension on this page to begin using it!
+					</p>
+				)}
 			</div>
-		</>
-	);
+		);
+
+		return (
+			<>
+				<style jsx global>{`
+					body {
+						font-family: 'Akaya Telivigala', cursive;
+						background: #23272a;
+						color: #fff;
+						display: inline-block;
+						margin: 0px auto;
+						text-align: center;
+					}
+
+					.twitchLogin {
+						-ms-transform: translate(-50%, -50%);
+						transform: translate(-50%, -50%);
+						transition: background-color 0.125s ease-in-out;
+						transform-origin: center center;
+						margin: 0;
+						position: absolute;
+						top: 50%;
+						left: 50%;
+
+						font-size: xx-large;
+					}
+
+					button {
+						font-family: 'Roboto Mono', monospace;
+						border-style: solid;
+						font-size: xx-large;
+						border-radius: 5px;
+						border-color: #9146ff;
+						background-color: #f0f0ff;
+					}
+
+					button:hover {
+						background-color: #383f5a;
+					}
+				`}</style>
+				<div>
+					This is in pre-pre-pre-pre-pre production. If you are not
+					jamesinaxx, don't use this...
+					<Button />
+				</div>
+			</>
+		);
+	}
 }
 
 export async function getStaticProps() {
 	return {
 		props: {
 			clientId: process.env.TWITCHCLIENT,
-            redirectUrl: process.env.REDICRECT_URL
+			redirectUrl: process.env.REDIRECT_URL,
 		},
 	};
 }
