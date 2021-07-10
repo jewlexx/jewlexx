bbf29a5f31d34fc2ae0d942eeefb59b7feb1e38a
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-10 15:15:06 +1000

    Removed a butload of old stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,6 @@
 import React from 'react';
-import Layout from '../components/Layout/_Main';
-import { connectToDatabase } from '../util/mongodb';
+import ComingSoon from '../components/ComingSoon';
 
-export default function Main({ isConnected }) {
-	return (
-		<Layout>
-			<div>This entire site is a work in progress...</div>
-			{!isConnected ? (
-				<div>
-					Failed to connect to database. Parts of this website may not
-					work properly
-				</div>
-			) : (
-				<div>{null}</div>
-			)}
-		</Layout>
-	);
-}
-
-export async function getServerSideProps(context) {
-	let isConnected;
-	try {
-		const { client } = await connectToDatabase();
-
-		isConnected = client.isConnected();
-	} catch (error) {
-		isConnected = false;
-		console.error(error);
-	}
-
-	return {
-		props: { isConnected },
-	};
+export default function Main() {
+	return <ComingSoon />;
 }
