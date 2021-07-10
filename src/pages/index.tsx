d78c0819369319d27805f487d4f051f612e09dd1
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-10 15:46:36 +1000

    Improved perf massively

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
-import ComingSoon from '../components/ComingSoon';
+import Head from 'next/head';
+import styles from '../styles/Soon.module.scss';
 
 export default function Main() {
-	return <ComingSoon />;
+	return (
+		<>
+			<Head>
+				<meta charSet='UTF-8' />
+				<meta
+					name='description'
+					content='Coming Soon! In the meantime, https://twitch.tv/jamesinaxx'
+				/>
+				<meta name='author' content='jamesinaxx' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
+				<title>Coming Soon!</title>
+			</Head>
+			<div className={styles.bgimg}>
+				<div className={styles.middle}>
+					<h1>COMING SOON</h1>
+					<hr className={styles.divider} />
+					<p>{'Date is also coming soon... Maybe'}</p>
+				</div>
+			</div>
+		</>
+	);
 }
