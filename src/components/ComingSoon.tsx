d91eb6c8912e2db02b417e1e7aa2b47e926ad78c
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-07-10 15:18:54 +1000

    Updated coming soon page

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBackward } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/Soon.module.scss';
 
 export default function ComingSoon({ date }: { date?: string }) {
 	return (
-		<div className='bgimg'>
-			<div className='middle'>
+		<div className={styles.bgimg}>
+			<div className={styles.middle}>
 				<h1>COMING SOON</h1>
 				<hr />
 				<p>{date || 'Date is also coming soon... Maybe'}</p>
 			</div>
-			<div className='bottomleft'>
-				<a className='goBack' href='/'>
+			<div className={styles.bottomleft}>
+				<a className={styles.goBack} href='/'>
 					<p>{<FontAwesomeIcon icon={faBackward} />} Go Back</p>
 				</a>
 			</div>
@@ -19,38 +20,10 @@ export default function ComingSoon({ date }: { date?: string }) {
 					user-select: none;
 				}
 
-				.goBack {
-					cursor: pointer;
-					color: #000;
-				}
-
 				body,
 				html {
 					height: 100%;
 					background: #000;
-				}
-
-				/* Position text in the top-left corner */
-				.topleft {
-					position: absolute;
-					top: 0;
-					left: 16px;
-				}
-
-				/* Position text in the bottom-left corner */
-				.bottomleft {
-					position: absolute;
-					bottom: 0;
-					left: 16px;
-				}
-
-				/* Position text in the middle */
-				.middle {
-					position: absolute;
-					top: 50%;
-					left: 50%;
-					transform: translate(-50%, -50%);
-					text-align: center;
 				}
 
 				/* Style the <hr> element */
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,5 @@
 import '../styles/global.scss';
-import dayjs from 'dayjs';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-import { UserProvider } from '@auth0/nextjs-auth0';
 
 export default function App({ Component, pageProps }) {
-	dayjs.extend(advancedFormat);
-	dayjs.extend(localizedFormat);
-
-	return (
-		<UserProvider>
-			<Component {...pageProps} />
-		</UserProvider>
-	);
+	return <Component {...pageProps} />;
 }
