aa042c3cb2f87ab528d7c7fd8087b7d346a759a2
James Cordor <61040004+jamesinaxx@users.noreply.github.com>
2021-09-05 22:05:07 +1000

    Delete code directory

--- code/javascripts/codewars/isogram.js
+++
@@ -1,8 +0,0 @@
-/**
-
- * Checks if the given string is an isogram (there are no repeating letters)
- * @param {string} str
- */
-const isIsogram = str => str.split('').every((c, i) => str.indexOf(c) == i);
-
-export default isIsogram;
